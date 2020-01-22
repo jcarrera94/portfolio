028f60b30e527e4196e4fb1a7f6ec3464079d804
Juan Carrera <juankarrera@gmail.com>
2020-01-21 23:32:31 -0500

    fix semantics in header component

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,14 @@ const Header = () => {
   return (
     <header>
       <h1>Juan Carrera's Portfolio</h1>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
     </header>
   )
 }
