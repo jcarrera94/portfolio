0226063b0decb629ef9b52f3d6b03c59b4f73a8b
Juan Carrera <juankarrera@gmail.com>
2020-01-22 23:51:29 -0500

    add styles to header and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+
+import layoutStyles from './layout.module.scss'
 import '../styles/index.scss'
 
 const Layout = (props) => {
   return (
-    <div>
-      <Header/>
-      {props.children}
-      <Footer/>
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
+      <Footer />
     </div>
   )
 }
