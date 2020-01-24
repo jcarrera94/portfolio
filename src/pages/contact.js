5c045cdbc30c0707eb87db559a8bc7cb050c7bd5
Juan Carrera <juankarrera@gmail.com>
2020-01-23 23:00:30 -0500

    fix contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import Layout from '../components/layout';
 
 const Contact = () => {
   return (
-    <div>
-      <Header/>
+    <Layout>
       <h1>Contact</h1>
       <p>email: juankarrera@gmail.com</p>
       <p>phone: +1 226 203 0096</p>
-      <Footer/>
-    </div>
+    </Layout>
   )
 }
 
