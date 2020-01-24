2673288686171d8f470566b564ceb9e8739d1d16
Juan Carrera <juankarrera@gmail.com>
2020-01-23 23:42:45 -0500

    use graphql to fetch site metadata to use in header and footer components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `JC Portfolio`,
+    author: 'Juan Carrera'
+  },
   /* Your site config here */
   plugins: [
     'gatsby-plugin-sass'
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
   return (
     <footer>
-      <p>Created by Juan Carrera, © {new Date().getFullYear()}</p>
+      <p>Created by {data.site.siteMetadata.author}, © {new Date().getFullYear()}</p>
     </footer>
   )
 }
